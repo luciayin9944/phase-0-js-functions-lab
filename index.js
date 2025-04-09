f071a6b1c879b419e13ace44f527dcd2dd4e9ece
luciayin9944 <luxiayin94@gmail.com>
2025-04-09 16:53:56 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+function calculateTax(amount){
+    let tax = amount * 0.10;
+    return tax;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2){
+    return Math.max(num1, num2);
+}
+
+function isPalindrome(word){
+    let lowWord = word.toLowerCase();
+    let reverseWord = lowWord.split('').reverse().join('');
+    if (lowWord === reverseWord){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    let price = originalPrice * (100 - discountPercentage)/100
+    return price
+}
 
 
 
